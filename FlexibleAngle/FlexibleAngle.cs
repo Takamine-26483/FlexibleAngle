27adab12c94a3e4fe3c7d9e3284beab6b56f80e5
Takamine-26483 <twice.cake.1379@gmail.com>
2021-09-14 20:41:09 +0900

    プロジェクト ファイルを追加します。

---
+++ FlexibleAngle/FlexibleAngle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakamineProduction
+{
+	/// <summary>保持している角度(double型)を度・ラジアン間で相互変換できる角度クラス</summary>
+    public class FlexibleAngle
+	{
+		private double _degree = 0;
+		private double _radian = 0;
+
+		/// <summary>保持している角度を度（°）として扱う</summary>
+		public double Degree
+		{
+			get => _degree;
+			set => CalcCore(value, false);
+		}
+
+		/// <summary>保持している角度をラジアン（rad）として扱う</summary>
+		public double Radian
+		{
+			get => _radian;
+			set => CalcCore(value, true);
+		}
+
+		/// <summary>角度変更時、必ず0~359°（Rad換算でおよそ0~PI*2）に収めるよう調整するフラグ</summary>
+		public bool IsMinimumContorlled { get; set; }
+
+
+		private void CalcCore(in double val, bool val_is_rad)
+		{
+			_degree = _radian = val;
+
+			switch (val)
+			{
+				case double.NaN:
+				case double.PositiveInfinity:
+				case double.NegativeInfinity:
+					return;	//★　break ではないので注意
+
+				default:
+					if (val_is_rad)
+						_degree = val * (180 / Math.PI);
+					else
+						_radian = val * (Math.PI / 180);
+					break;
+			}
+
+			if (IsMinimumContorlled)
+			{
+				_degree = Math.Abs(_degree) % 360;
+				_radian = _degree * (Math.PI / 180);
+			}
+		}
+		
+	}
+}
---
+++ Sample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TakamineProduction;
+
+
+
+namespace Sample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var angle = new FlexibleAngle();
+
+
+			Write("★初期状態");
+
+			angle.Degree = 180;
+			Write("★Degree = 180");
+
+			angle.Radian = 1;
+			Write("★Radian = 1");
+
+			angle.Radian = double.NaN;
+			Write("★Radian = double.NaN");
+
+			angle.Radian = Math.PI * 2;
+			Write("★Radian = Math.PI * 2");
+
+			angle.Degree += 45;
+			Write("★Degree += 45");
+
+			angle.IsMinimumContorlled = true;
+			angle.Degree = 360;
+			Write("★Degree = 360");
+
+			angle.Degree = -90;
+			Write("★Degree = -90");
+
+			Console.ReadKey();
+
+
+			//************************************************************************************
+
+			void Write(string title)
+			{
+				Console.WriteLine(title);
+				Console.WriteLine("Degree:" + angle.Degree.ToString());
+				Console.WriteLine("Radian:" + angle.Radian.ToString());
+				Console.WriteLine("");
+			}
+		}
+	}
+}
